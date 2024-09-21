bc238333486dfd016931306c0c323b971d1e9f87
mafinovich <mafinovich@proton.me>
2024-09-21 18:24:25 +0500

    Добавлен класс /setCommander

---
+++ src/main/java/com/embrodev/Commands/setCommander.java
@@ -0,0 +1,45 @@
+package com.embrodev.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class setCommander implements CommandExecutor {
+    public static Player attack_commander;
+    public static Player defense_commander;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+        String cmdName = command.getName().toLowerCase();
+        Player target = Bukkit.getPlayerExact(args[0]);
+
+
+        if(cmdName.equals("setcommander")){
+            if(args.length < 1){
+                p.sendMessage("Укажите ник командира и команду(attack, defense)");
+                return false;
+            } else if (args.length == 1) {
+                p.sendMessage("Укажите команду(attack, defense)");
+                return false;
+            } else if (!(args[1].equals("attack") || args[1].equals("defense") )) {
+                p.sendMessage("Укажите команду в правильном формате!(attack, defense)");
+                return false;
+            }
+
+            if(p.hasPermission("elegantwar.setcommander")){
+                if(args[1].equals("attack")) {
+                    attack_commander = target;
+                    p.sendMessage("Игрок " + target + " назначен командиром атакующей стороны!");
+                } else{
+                    defense_commander = target;
+                    p.sendMessage("Игрок " + target + "назначен командиром стороны защиты!");
+                }
+            }
+        }
+
+        return true;
+    }
+}
